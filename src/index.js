b693f0e2b0477aa9873bee9a3f2c9629db3fbcae
Edgar Montes <jedgar1mx@gmail.com>
2023-07-19 15:00:33 -0400

    Moving COD UXDX dependency to only dev testing.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,2 @@
 import RecyclingChallenge from './components/RecyclingChallenge';
-import 'cod-design-system/src/components/atoms/Button/cod-button';
-import 'cod-design-system/src/components/atoms/Icon/cod-icon';
 customElements.define('recycling-challenge', RecyclingChallenge);
